a210dd306aeead9c448131b70968d1f0c49b5953
Alejandra <alejandra.e.bernabel@gmail.com>
2023-11-11 01:34:10 -0500

    Added user and post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Post } = require ('../../models');
+
+//the `/api/posts` endpoint
+
+// to get all posts
+
+router.get('/', (req, res) => {
+    Post.findAll()
+    .then((posts) => res.json(posts))
+    .catch((err)=> {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//get one post
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        }
+    })
+    .then((posts) => res.json(posts))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//create new post
+
+router.post('/', (req, res) => {
+    Post.create(req.body)
+        .then((post) => res.status(200).json(post))
+        .catch((err) => res.status(400).json(err));
+});
+
+
+//update a post
+router.put('/:id', (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((post)=> res.status(200).json(post))
+    .catch((err)=> res.status(400).json(err));
+});
+
+//delete a post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((post) => res.status(200).json(post))
+    .catch((err)=> res.status(400).json(err));
+});
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { User } = require ('../../models');
+
+//the `/api/users` endpoint
+
+// to get all users
+
+router.get('/', (req, res) => {
+    User.findAll()
+    .then((users) => res.json(users))
+    .catch((err)=> {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//get one user
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id,
+        }
+    })
+    .then((users) => res.json(users))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//create new user
+
+router.post('/', (req, res) => {
+    User.create(req.body)
+        .then((user) => res.status(200).json(user))
+        .catch((err) => res.status(400).json(err));
+});
+
+
+//update a user
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((user)=> res.status(200).json(user))
+    .catch((err)=> res.status(400).json(err));
+});
+
+//delete a user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((user) => res.status(200).json(user))
+    .catch((err)=> res.status(400).json(err));
+});
