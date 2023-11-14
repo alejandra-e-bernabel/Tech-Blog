6bbe99c939582c03c3ab87677c5a3a63e4d12531
Alejandra <alejandra.e.bernabel@gmail.com>
2023-11-13 19:58:07 -0500

    Worked on front end for login and signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,36 @@
-
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
     const username = document.getElementById('username-signup').value.trim();
     const email = document.getElementById('email-signup').value.trim();
     const password = document.getElementById('password-signup').value.trim();
+    const confirmPassword = document.getElementById('password-confirm').value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users/signup', {
-            method: 'POST',
-            body: JSON.stringify({ username, email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            //message for a failed signup, specifically needs changing to specify the error
-            alert('Failed to sign up.');
+    if (password === confirmPassword) {
+        if (username && email && password) {
+            const response = await fetch('/api/users/signup', {
+                method: 'POST',
+                body: JSON.stringify({ username, email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+    
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                //message for a failed signup, specifically needs changing to specify the error
+                alert('Failed to sign up.');
+            }
         }
+    } else {
+        const noPasswordMatchEl = document.getElementById('noPasswordMatchEl');
+
+        noPasswordMatchEl.classList.remove("hidden");
+        window.setTimeout(function(){
+            noPasswordMatchEl.classList.add("hidden");
+            // noPasswordMatchEl.classList.add()
+          }, 10000);
     }
+    
 };
 
 const handleLogin = () => document.location.replace('/login');
